50698f57a6ff35762fbff34f57b6e8ec4ba7c77d
Vasilena Georgieva <75674763+vassiyanakiev@users.noreply.github.com>
2021-06-16 15:38:25 +0300

    username chacker

---
+++ ValidUsername/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace ValidUsername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var username in usernames)
+            {
+                if (IsValid(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+
+        private static bool IsValid(string username)
+        {
+            return IsValidLenght(username) && ContaingValidSymbols(username);
+        }
+
+        private static bool ContaingValidSymbols(string username)
+        {
+            foreach (char symbol in username)
+            {
+                if (!char.IsLetterOrDigit(symbol) && symbol != '_' && symbol != '-')
+                {
+                    return false;
+                }
+               
+            }
+
+            return true;
+        }
+
+        private static bool IsValidLenght(string username)
+        {
+            return username.Length >= 3 && username.Length <= 16;
+        }
+    }
+}
